14b7dec34c1bb2a5c095904a3a8fc48ddfa40a9d
anmol7240 <anmolshooter7240@gmail.com>
2026-09-22 01:56:57 +0530

    Add prediction API test

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,3 +14,18 @@ def test_db_health():
     response = client.get("/db-health")
 
     assert response.status_code == 200
+
+def test_predict_category():
+    response = client.post(
+        "/predict-category",
+        params={
+            "message": "My payment was deducted"
+        }
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert "predicted_category" in data
+    assert isinstance(data["predicted_category"], str)
